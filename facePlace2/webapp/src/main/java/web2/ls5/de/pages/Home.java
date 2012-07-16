5526fa645ee9ccd01a0bcc9e8dc12ced9ad282ec
christoph2.schuermann <christoph2.schuermann@gmail.com>
2012-07-16 15:11:38 +0000

    Home-Seite des users als skelett aufgebaut.

--- facePlace2/webapp/src/main/java/web2/ls5/de/pages/Home.java
+++ facePlace2/webapp/src/main/java/web2/ls5/de/pages/Home.java
@@ -1,13 +1,17 @@
 package web2.ls5.de.pages;
 
+import java.util.Set;
+
 import org.apache.tapestry5.*;
 import org.apache.tapestry5.annotations.*;
 import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.SymbolConstants;
 
+import web2.ls5.de.backend.ApplicationBackend;
 import web2.ls5.de.components.Layout;
 import web2.ls5.de.entities.DBPerson;
+import web2.ls5.de.entities.DBPost;
 
 /**
  * Page for Userhome. Contains other components.
@@ -18,6 +22,18 @@ public class Home
 	@Property
 	@SessionState(create=false)
 	private DBPerson loggedInPerson;
+	
+	@Inject
+	private ApplicationBackend backend;
+	
+	@Property
+	private DBPost post;
+	
+	public Set<DBPost> getPosts()
+	{
+		return backend.getAllPosts();
+	}
+	
 	
 	
 }
