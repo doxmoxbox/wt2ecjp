511d85612b6475d9bff40ecf077de9433f9c282e
christoph2.schuermann <christoph2.schuermann@gmail.com>
2012-07-16 20:32:22 +0000

    User ist von nun an eingeloggt wenn er sich zum ersten mal registriert.

--- facePlace2/webapp/src/main/java/web2/ls5/de/components/RegisterComponent.java
+++ facePlace2/webapp/src/main/java/web2/ls5/de/components/RegisterComponent.java
@@ -5,12 +5,14 @@ import java.util.Date;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.corelib.components.PasswordField;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import web2.ls5.de.backend.ApplicationBackend;
 import web2.ls5.de.backend.UserManager;
+import web2.ls5.de.entities.DBPerson;
 
 public class RegisterComponent 
 {
@@ -30,13 +32,24 @@ public class RegisterComponent
   private UserManager authenticator = backend.getUserManager();
   @Component(id = "registerPassword")
   private PasswordField registerPasswordField;
+  
+  @SessionState
+  private DBPerson loggedInPerson;
 
   /**
    * Creates a new user.
    */
-  void onValidateFromRegisterForm() {
-    if (!authenticator.signUp(name, password, repeatPassword, birthdate)) {
-      registerForm.recordError(registerPasswordField, "Passwords are not equal.");
+  void onValidateFromRegisterForm() 
+  {
+    if (!authenticator.signUp(name, password, repeatPassword, birthdate))
+    {
+    	registerForm.recordError(registerPasswordField, "Passwords are not equal.");
+    }
+    
+    loggedInPerson = authenticator.isValid(name, password);
+    if(loggedInPerson == null)
+    {
+    	registerForm.recordError(registerPasswordField, "Login Failed! Try again separatly.");
     }
   }
 
